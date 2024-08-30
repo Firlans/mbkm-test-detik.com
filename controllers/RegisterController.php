13273b96798d586f4c1f43f14fe6031b0b0ef18c
firlans <firlansyah54321@gmail.com>
2024-08-30 15:22:51 +0700

    fixing bug process

--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'models/User.php';
 
-class RegisterController{
+class RegisterController
+{
     private $conn;
     private $userModel;
     public function __construct($conn)
@@ -9,24 +10,24 @@ class RegisterController{
         $this->conn = $conn;
         $this->userModel = new User($conn);
     }
-    
-    public function register($username, $email, $password, $confirm_password){
-        if(!$username || !$email || !$password || !$confirm_password){
+
+    public function register($username, $email, $password, $confirm_password)
+    {
+        if (!$username || !$email || !$password || !$confirm_password) {
             return "harap isi semua field";
         }
-        if($password !== $confirm_password){
+        if ($password !== $confirm_password) {
             return 'confirm password tidak sesuai';
         }
-        $isAvailableEmail = $this->userModel->getUserByEmail($email);
-        if($isAvailableEmail){
-            return "email sudah digunakan, silakan login ke email $email";
+        if ($this->userModel->getUserByEmail($email)) {
+            return "email sudak digunakan, silakan login ke email $email";
         }
         $role = 'user';
         $isRegister = $this->userModel->createUser($username, $email, $password, $role);
-        if(!$isRegister){
-            return "terjadi, silakan coba lagi";
+        if ($isRegister) {
+            return 'sukses';
         }
-        return 'sukses';
+        return "terjadi, silakan coba lagi";
     }
 }
 ?>
